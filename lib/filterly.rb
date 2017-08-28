148c1d5872115ca604d0cbd9d8e59192146389e3
Tomek <tomeroth@op.pl>
2017-08-28 09:56:30 +0200

    Add default requires on filterly module

--- lib/filterly.rb
+++ lib/filterly.rb
@@ -1,5 +1,8 @@
-require "filterly/version"
 require 'ast'
+require 'filterly/version'
+require 'filterly/parser'
+require 'filterly/tree'
+require 'filterly/node_builder'
 
 module Filterly
   # Your code goes here...
